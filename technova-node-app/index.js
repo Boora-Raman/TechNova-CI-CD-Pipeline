984a327da4335e67e62ef89af2f02719223e07e1
Boora-Raman <157504430+Boora-Raman@users.noreply.github.com>
2025-06-25 10:35:42 +0530

    Update index.js

--- technova-node-app/index.js
+++ technova-node-app/index.js
@@ -4,7 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// Serve static HTML page
+// Serve static HTML page 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
