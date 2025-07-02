d82b567ff10377c794ced6bedfae27a0c230a668
Boora-Raman <157504430+Boora-Raman@users.noreply.github.com>
2025-07-03 00:34:24 +0530

    Update main.tf

---
+++ Terraform-Deployment/main.tf
@@ -0,0 +1,34 @@
+ provider "aws" {
+  region = "us-east-2"
+}
+
+module "Vpc-ubnets" {
+  source = "./VPC & SG"  
+}   
+ 
+module "Iam_roles" { 
+  source = "./IAM & Roles" 
+}
+
+module "alb" {
+  source            = "./ALB"
+  vpc_id            = module.Vpc-ubnets.vpc_id
+  subnet_ids        = module.Vpc-ubnets.subnet_ids
+  security_group_id = toset([module.Vpc-ubnets.allow_tls_sg_id])
+}
+
+module "ecs" {
+  source            = "./ECS-Cluster"
+  execution_role_arn = module.Iam_roles.iam-role
+  target_group_arn  = module.alb.blue-target_group_arn
+  subnet_ids        = module.Vpc-ubnets.subnet_ids
+  security_group_id = toset([module.Vpc-ubnets.allow_tls_sg_id])
+  vpc_id            = module.Vpc-ubnets.vpc_id
+  depends_on = [module.alb]
+}
+
+
+output "dns" {
+    value = module.alb.alb_dns_name
+}
+  
