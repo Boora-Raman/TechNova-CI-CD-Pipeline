b3b6f3d80399d3f99acc5a780be8345cd87f2a37
Boora-Raman <157504430+Boora-Raman@users.noreply.github.com>
2025-07-09 02:04:42 +0530

    Update alb.tf

--- Terraform-Deployment/ALB/alb.tf
+++ Terraform-Deployment/ALB/alb.tf
@@ -1,41 +1,66 @@
+# Application Load Balancer
 resource "aws_lb" "test" {
   name               = "technova-lb-tf"
   internal           = false
   load_balancer_type = "application"
   security_groups    = var.security_group_id
   subnets            = var.subnet_ids
+
   tags = {
     Environment = "production"
+    Name        = "TechnovaALB"
   }
 }
 
-variable "lb_target_group_name" { default = "technova-ecs-tg" }
-
-locals {
-  target_groups = ["blue", "green"]
-}
-
-resource "aws_lb_target_group" "TG" {
-  
-   count       = length(local.target_groups)
-  name        = "${var.lb_target_group_name}-${local.target_groups[count.index]}"
-  port        = 3000
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"
-  deregistration_delay = "80"
+# Blue Target Group
+resource "aws_lb_target_group" "technova_blue_tg" {
+  name                 = "technova-ecs-tg-blue"
+  port                 = 3000
+  protocol             = "HTTP"
+  vpc_id               = var.vpc_id
+  target_type          = "ip"
+  deregistration_delay = 80
 
   health_check {
-    path                = "/"  # Verify path
+    path                = "/"
     protocol            = "HTTP"
-    port = "3000"
+    port                = "3000"
     interval            = 50
     timeout             = 30
     healthy_threshold   = 2
     unhealthy_threshold = 3
   }
+
+  tags = {
+    Name = "TechnovaBlueTargetGroup"
+  }
 }
 
+# Green Target Group
+resource "aws_lb_target_group" "technova_green_tg" {
+  name                 = "technova-ecs-tg-green"
+  port                 = 3000
+  protocol             = "HTTP"
+  vpc_id               = var.vpc_id
+  target_type          = "ip"
+  deregistration_delay = 80
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    port                = "3000"
+    interval            = 50
+    timeout             = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name = "TechnovaGreenTargetGroup"
+  }
+}
+
+# ALB Listener (Initially forwards to Blue Target Group)
 resource "aws_lb_listener" "Listener" {
   load_balancer_arn = aws_lb.test.arn
   port              = 80
@@ -43,8 +68,10 @@ resource "aws_lb_listener" "Listener" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.TG[0].arn
-  
+    target_group_arn = aws_lb_target_group.technova_blue_tg.arn
+  }
+
+  tags = {
+    Name = "TechnovaHTTPListener"
   }
 }
-
